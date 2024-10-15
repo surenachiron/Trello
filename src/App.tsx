a8454cfd6242c6a65781e63b69b56145f16a9cdf
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-16 00:02:42 +0330

    feat: add header and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Home from "./Pages/Home";
-import Auth from "./Pages/Auth";
+import Home from "@/pages/Home";
+import Auth from "@/pages/Auth";
+import Header from "@/components/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
+    <div className="font-open_sans">
+      <Header />
+      <div className="container py-2 md:py-5">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
     </div>
   );
 }

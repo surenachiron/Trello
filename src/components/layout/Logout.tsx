fa85b001351f4331e33eecd4b50f1fada55712d1
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-16 00:08:10 +0330

    feat: add initial logout component

---
+++ src/components/layout/Logout.tsx
@@ -0,0 +1,13 @@
+import { IoLogOutOutline } from "react-icons/io5";
+
+const Logout = () => {
+  return (
+    <div>
+      <button className="rounded-full py-1">
+        <IoLogOutOutline size={25} />
+      </button>
+    </div>
+  );
+};
+
+export default Logout;

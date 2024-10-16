a5ca47e3378d756c207659413f7d206cdd0bb81f
mohammad <mohammadkarimi.swctw@gmai.com>
2024-10-16 17:55:34 +0330

    chore(craco_config): update paths

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,13 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
+      "@/components/*": ["./src/components/*"],
+      "@/pages/*": ["./src/pages/*"],
+      "@/routes/*": ["./src/routes/*"],
+      "@/lib/*": ["./src/lib/*"],
+      "@/store/*": ["./src/store/*"],
+      "@/hooks/*": ["./src/hooks/*"],
     },
   },
 };
